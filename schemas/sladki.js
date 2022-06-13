10576d8b42ae3f3d7bb674b717550176722e816e
Joszeph <iodesign@live.com>
2022-06-13 19:01:05 +0300

    first commit

---
+++ schemas/sladki.js
@@ -0,0 +1,37 @@
+export default {
+    name: 'sladki',
+    title: 'Сладкарски изделия',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        description: "Име на продукта",
+        type: 'string',
+      },
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        description: "Не се попълва нищо в полето, а се избира се GENERATE.",
+        options: {
+          source: 'title',
+          maxLength: 96,
+        },
+      },
+      {
+        name: 'mainImage',
+        title: 'Main image',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: 'body',
+        title: 'Body',
+        type: 'blockContent',
+      },
+    ],
+  }
+  
